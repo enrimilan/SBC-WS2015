df87630639bd867de583a383538b9f474baef1fd
enrimilan <enrimiho@gmail.com>
2015-11-23 19:46:35 +0100

    Make use of the logger.

--- src/main/java/at/ac/tuwien/gui/Main.java
+++ src/main/java/at/ac/tuwien/gui/Main.java
@@ -2,10 +2,15 @@ package at.ac.tuwien.gui;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main extends Application {
 
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        logger.info("Application started.");
         launch(args);
     }
 
