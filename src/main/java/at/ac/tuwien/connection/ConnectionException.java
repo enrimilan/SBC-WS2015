dd7ac7cfd2e356285dc266cf837a93f88a34df58
enrimilan <enrimiho@gmail.com>
2015-11-27 21:52:16 +0100

    Create an own connection exception for connection related exceptions

---
+++ src/main/java/at/ac/tuwien/connection/ConnectionException.java
@@ -0,0 +1,11 @@
+package at.ac.tuwien.connection;
+
+public class ConnectionException extends Exception {
+    public ConnectionException() {
+        super();
+    }
+
+    public ConnectionException(String message) {
+        super(message);
+    }
+}
