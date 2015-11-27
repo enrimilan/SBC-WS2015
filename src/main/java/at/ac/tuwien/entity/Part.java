83dc1a065b8818b824cc2356d54c47328558d03a
enrimilan <enrimiho@gmail.com>
2015-11-27 21:35:54 +0100

    Create enum for the drone parts.

---
+++ src/main/java/at/ac/tuwien/entity/Part.java
@@ -0,0 +1,30 @@
+package at.ac.tuwien.entity;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+public class Part implements Serializable {
+
+    private UUID partId;
+    private UUID supplierId;
+    private PartType partType;
+
+    public Part(UUID supplierId, PartType partType) {
+        this.partId = UUID.randomUUID();
+        this.supplierId = supplierId;
+        this.partType = partType;
+    }
+
+    public UUID getPartId() {
+        return partId;
+    }
+
+    public UUID getSupplierId() {
+        return supplierId;
+    }
+
+    public PartType getPartType() {
+        return partType;
+    }
+}
+
