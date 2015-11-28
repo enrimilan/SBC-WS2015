ce07cd112384b0ca904345398daf9e47aec41713
enrimilan <enrimiho@gmail.com>
2015-11-28 20:45:37 +0100

    the notification must also extend the remote interface.

--- src/main/java/at/ac/tuwien/robot/IAssemblyRobotNotification.java
+++ src/main/java/at/ac/tuwien/robot/IAssemblyRobotNotification.java
@@ -3,11 +3,13 @@ package at.ac.tuwien.robot;
 import at.ac.tuwien.entity.Module;
 import at.ac.tuwien.entity.Part;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 
-public interface IAssemblyRobotNotification {
+public interface IAssemblyRobotNotification extends Remote {
 
-    void assembleMotorRotorPairs(ArrayList<Part> motors, ArrayList<Part> rotors);
-    void assembleCaseControlUnitPair(Part casePart, Part controlUnit);
-    void assembleDrone(Module caseControlUnitPair, ArrayList<Module> motorRotorPairs);
+    void assembleMotorRotorPairs(ArrayList<Part> motors, ArrayList<Part> rotors) throws RemoteException;
+    void assembleCaseControlUnitPair(Part casePart, Part controlUnit) throws RemoteException;
+    void assembleDrone(Module caseControlUnitPair, ArrayList<Module> motorRotorPairs) throws RemoteException;
 }
