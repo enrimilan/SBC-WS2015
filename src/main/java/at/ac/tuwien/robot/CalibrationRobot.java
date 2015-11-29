d4b15c8b4b334400148d5349e280fcbe7ba23cbd
enrimilan <enrimiho@gmail.com>
2015-11-29 12:25:06 +0100

    create the calibration robot

---
+++ src/main/java/at/ac/tuwien/robot/CalibrationRobot.java
@@ -0,0 +1,60 @@
+package at.ac.tuwien.robot;
+
+import at.ac.tuwien.connection.Connection;
+import at.ac.tuwien.connection.ConnectionException;
+import at.ac.tuwien.entity.Drone;
+import at.ac.tuwien.entity.Module;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.UUID;
+
+public class CalibrationRobot extends UnicastRemoteObject implements Runnable, ICalibrationRobotNotification, Serializable {
+
+    private final static Logger logger = LoggerFactory.getLogger(CalibrationRobot.class);
+    private final static int INTERVAL = 1000;
+    private Connection connection;
+    private UUID id;
+    private CalibrationRobot calibrationRobot;
+
+    public CalibrationRobot() throws RemoteException, ConnectionException {
+        super();
+        this.connection = new Connection();
+        connection.establish();
+        this.id = UUID.randomUUID();
+        this.calibrationRobot = this;
+    }
+
+    @Override
+    public void calibrateMotorRotorPair(Module module) throws RemoteException {
+
+    }
+
+    @Override
+    public void calibrateModuleInDrone(Drone drone) throws RemoteException {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public static void main(String[] args){
+        if(args.length != 0){
+            throw new IllegalArgumentException("Usage: CalibrationRobot");
+        }
+
+        try {
+            CalibrationRobot calibrationRobot = new CalibrationRobot();
+            calibrationRobot.run();
+        } catch (ConnectionException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+}
