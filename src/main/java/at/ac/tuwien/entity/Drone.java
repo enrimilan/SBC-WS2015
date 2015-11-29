b3e4f4ffc8269ddf27fe5eb631dd0179728ff1d2
enrimilan <enrimiho@gmail.com>
2015-11-29 12:19:16 +0100

    the drone must also have a status

--- src/main/java/at/ac/tuwien/entity/Drone.java
+++ src/main/java/at/ac/tuwien/entity/Drone.java
@@ -9,10 +9,12 @@ public class Drone implements Serializable {
     private Module caseControlUnitPair;
     private ArrayList<Module> motorRotorPairs;
     private UUID assemblerId;
+    private Status status;
 
     public Drone(Module caseControlUnitPair, UUID assemblerId) {
         this.caseControlUnitPair = caseControlUnitPair;
         this.assemblerId = assemblerId;
+        this.status = Status.ASSEMBLED;
     }
 
     public Module getCaseControlUnitPairs(){
@@ -27,12 +29,21 @@ public class Drone implements Serializable {
         this.motorRotorPairs = motorRotorPairs;
     }
 
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
     @Override
     public String toString() {
         return "Drone{" +
                 "caseControlUnitPair=" + caseControlUnitPair +
                 ", motorRotorPairs=" + motorRotorPairs +
                 ", assemblerId=" + assemblerId +
+                ", status=" + status +
                 '}';
     }
 }
