9be5e66717af26aed5da81b11b473eebe7938e2b
enrimilan <enrimiho@gmail.com>
2015-12-10 22:56:11 +0100

    add some utils

--- src/main/java/at/ac/tuwien/utils/Constants.java
+++ src/main/java/at/ac/tuwien/utils/Constants.java
@@ -2,5 +2,10 @@ package at.ac.tuwien.utils;
 
 public class Constants {
 
+    public final static String SERVER_HOST = "localhost";
     public final static int SERVER_PORT = 1337;
+    public final static String MOTORS_CONTAINER_NAME = "motors";
+    public final static String ROTORS_CONTAINER_NAME = "rotors";
+    public final static String CASES_CONTAINER_NAME = "cases";
+    public final static String CONTROL_UNITS_CONTAINER_NAME = "control_units";
 }
--- src/main/java/at/ac/tuwien/utils/Utils.java
+++ src/main/java/at/ac/tuwien/utils/Utils.java
@@ -4,8 +4,21 @@ import at.ac.tuwien.common.connection.IConnection;
 import at.ac.tuwien.rmi.RmiConnection;
 import at.ac.tuwien.common.entity.PartType;
 import at.ac.tuwien.xvsm.XVSMConnection;
+import org.mozartspaces.capi3.Coordinator;
+import org.mozartspaces.capi3.FifoCoordinator;
+import org.mozartspaces.core.Capi;
+import org.mozartspaces.core.ContainerReference;
+import org.mozartspaces.core.MzsConstants;
+import org.mozartspaces.core.MzsCoreException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+import java.util.ArrayList;
 
 public class Utils {
+
+    private static Logger logger = LoggerFactory.getLogger(Utils.class);
 
     public static PartType getPartType(String id){
         if(id.equals("A")){
@@ -33,4 +46,30 @@ public class Utils {
             return new XVSMConnection();
         }
     }
+
+
+    public static ContainerReference getOrCreateContainer(String containerName, Capi capi) {
+        ContainerReference cref = null;
+        URI spaceUri = getSpaceUri(Constants.SERVER_HOST, Constants.SERVER_PORT);
+        try {
+            cref = capi.lookupContainer(containerName, spaceUri, MzsConstants.RequestTimeout.DEFAULT, null);
+            logger.debug("created container: "+ containerName);
+        } catch (MzsCoreException e) {
+            // Container doesn't exist, so create a new one
+            ArrayList<Coordinator> obligatoryCoords = new ArrayList<>();
+            obligatoryCoords.add(new FifoCoordinator());
+            try {
+                logger.debug("Container " + containerName + "not found. Creating a new one");
+                cref = capi.createContainer(containerName, spaceUri, MzsConstants.Container.UNBOUNDED, obligatoryCoords, null, null);
+            } catch (MzsCoreException e1) {
+                logger.debug(e.getMessage());
+            }
+        }
+        return cref;
+    }
+
+    private static URI getSpaceUri(String host, int port) {
+        return URI.create("xvsm://" + host + ":" + port);
+    }
+
 }
