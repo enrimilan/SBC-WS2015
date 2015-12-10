774748903787b5ef95f01147587f940406dd9349
enrimilan <enrimiho@gmail.com>
2015-12-10 16:23:45 +0100

    start implementing the space solution

---
+++ src/main/java/at/ac/tuwien/MainXVSM.java
@@ -0,0 +1,11 @@
+package at.ac.tuwien;
+
+import at.ac.tuwien.xvsm.Server;
+
+public class MainXVSM {
+
+    public static void main(String[] args){
+        Server server = new Server();
+        server.start();
+    }
+}
--- src/main/java/at/ac/tuwien/xvsm/Server.java
+++ src/main/java/at/ac/tuwien/xvsm/Server.java
@@ -1,4 +1,25 @@
 package at.ac.tuwien.xvsm;
 
+import org.mozartspaces.core.Capi;
+import org.mozartspaces.core.DefaultMzsCore;
+import org.mozartspaces.core.MzsCore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Server {
+
+    private final static Logger logger = LoggerFactory.getLogger(Server.class);
+    private final static int PORT = 4444;
+    private MzsCore core;
+    private Capi capi;
+
+    public Server(){
+
+    }
+
+    public void start(){
+        this.core = DefaultMzsCore.newInstance(PORT);
+        this.capi = new Capi(core);
+        logger.debug("Server started");
+    }
 }
