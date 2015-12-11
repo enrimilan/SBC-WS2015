039a42fe2224b8185755f13a91c78f4f2d8b5874
enrimilan <enrimiho@gmail.com>
2015-12-11 12:35:09 +0100

    let the AbstractRobot class generate the id

--- src/main/java/at/ac/tuwien/common/robot/AbstractRobot.java
+++ src/main/java/at/ac/tuwien/common/robot/AbstractRobot.java
@@ -3,12 +3,16 @@ package at.ac.tuwien.common.robot;
 import at.ac.tuwien.common.connection.ConnectionException;
 import at.ac.tuwien.common.connection.IConnection;
 
+import java.util.UUID;
+
 public abstract class AbstractRobot {
 
     protected IConnection connection;
+    protected UUID id;
 
     public AbstractRobot(IConnection connection) throws ConnectionException {
         this.connection = connection;
+        this.id = UUID.randomUUID();
         connection.establish();
     }
 }
