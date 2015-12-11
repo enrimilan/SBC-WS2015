bd5e30b3965ce2bf8a5fb3c1f955e3e2b509ef07
enrimilan <enrimiho@gmail.com>
2015-12-11 17:06:33 +0100

    the RMIServer and the XVSMServer should implement this interface (this is for the GUI)

---
+++ src/main/java/at/ac/tuwien/common/server/IServer.java
@@ -0,0 +1,11 @@
+package at.ac.tuwien.common.server;
+
+import at.ac.tuwien.common.view.INotificationCallback;
+
+public interface IServer {
+
+    void registerGUINotificationCallback(INotificationCallback notificationCallback);
+    void start();
+    void stop();
+
+}
