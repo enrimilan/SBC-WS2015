06412b1848f06d40a18f6db52bf7cb435e7a33fd
enrimilan <enrimiho@gmail.com>
2015-12-11 12:42:01 +0100

    add constants for the notification containers

--- src/main/java/at/ac/tuwien/utils/Constants.java
+++ src/main/java/at/ac/tuwien/utils/Constants.java
@@ -9,4 +9,7 @@ public class Constants {
     public final static String ROTORS_CONTAINER_NAME = "rotors";
     public final static String CASES_CONTAINER_NAME = "cases";
     public final static String CONTROL_UNITS_CONTAINER_NAME = "control_units";
+    public final static String ASSEMBLED_NOTIFICATIONS = "assembled_notifications";
+    public final static String CALIBRATED_NOTIFICATIONS = "calibrated_notifications";
+    public final static String TESTED_NOTIFICATIONS = "tested_notifications";
 }
