b166e1bc0003af48a5e8463bf646e679eca69eb2
enrimilan <enrimiho@gmail.com>
2015-12-14 12:59:34 +0100

    add an aspect for listening to rollbacks.

---
+++ src/main/java/at/ac/tuwien/xvsm/aspect/SpaceAspect.java
@@ -0,0 +1,88 @@
+package at.ac.tuwien.xvsm.aspect;
+
+import at.ac.tuwien.common.entity.Drone;
+import at.ac.tuwien.common.entity.Module;
+import at.ac.tuwien.common.entity.Part;
+import at.ac.tuwien.common.view.INotificationCallback;
+import at.ac.tuwien.utils.Constants;
+import at.ac.tuwien.utils.Utils;
+import org.mozartspaces.capi3.*;
+import org.mozartspaces.core.Capi;
+import org.mozartspaces.core.ContainerReference;
+import org.mozartspaces.core.MzsConstants;
+import org.mozartspaces.core.MzsCoreException;
+import org.mozartspaces.core.aspects.AbstractSpaceAspect;
+import org.mozartspaces.core.aspects.AspectResult;
+import org.mozartspaces.core.requests.RollbackTransactionRequest;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by Enri on 12/14/2015.
+ */
+public class SpaceAspect extends AbstractSpaceAspect {
+
+    private Capi capi;
+    private INotificationCallback notificationCallback;
+    private ContainerReference partsContainer, modulesContainer, dronesContainer;
+
+    public SpaceAspect(Capi capi, INotificationCallback notificationCallback){
+        this.capi = capi;
+        this.notificationCallback = notificationCallback;
+        List<Coordinator> coordinators = new ArrayList<>();
+        coordinators.add(new QueryCoordinator());
+        coordinators.add(new FifoCoordinator());
+        this.partsContainer = Utils.getOrCreateContainer(Constants.PARTS_CONTAINER, capi, coordinators);
+        this.modulesContainer = Utils.getOrCreateContainer(Constants.MODULES_CONTAINER, capi, coordinators);
+        this.dronesContainer = Utils.getOrCreateContainer(Constants.DRONES_CONTAINER, capi, coordinators);
+    }
+
+    @Override
+    public AspectResult postRollbackTransaction(RollbackTransactionRequest request, Transaction tx) {
+
+        Collection<LocalContainerReference> containers = tx.getAccessedContainers();
+
+        for(LocalContainerReference l : containers){
+            if(partsContainer.getId().equals(l.getId())){
+                try {
+                    ArrayList<Part> parts = capi.read(
+                            partsContainer,
+                            FifoCoordinator.newSelector(MzsConstants.Selecting.COUNT_ALL),
+                            MzsConstants.RequestTimeout.DEFAULT,
+                            null);
+                    notificationCallback.setAllParts(parts);
+                } catch (MzsCoreException e) {
+                    e.printStackTrace();
+                }
+            }
+            else if(modulesContainer.getId().equals(l.getId())){
+                try {
+                    ArrayList<Module> modules = capi.read(
+                            modulesContainer,
+                            FifoCoordinator.newSelector(MzsConstants.Selecting.COUNT_ALL),
+                            MzsConstants.RequestTimeout.DEFAULT,
+                            null);
+                    notificationCallback.setAllModules(modules);
+                } catch (MzsCoreException e) {
+                    e.printStackTrace();
+                }
+            }
+            else if(partsContainer.getId().equals(l.getId())){
+                try {
+                    ArrayList<Drone> drones = capi.read(
+                            dronesContainer,
+                            FifoCoordinator.newSelector(MzsConstants.Selecting.COUNT_ALL),
+                            MzsConstants.RequestTimeout.DEFAULT,
+                            null);
+                    notificationCallback.setAllDrones(drones);
+                } catch (MzsCoreException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return AspectResult.OK;
+    }
+}
