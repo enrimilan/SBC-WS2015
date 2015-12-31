f085aee01c3e488bf452f458c8d28d5c6bd355f1
enrimilan <enrimiho@gmail.com>
2015-12-31 19:14:06 +0100

    add new properties to the part entity.

--- src/main/java/at/ac/tuwien/common/entity/Part.java
+++ src/main/java/at/ac/tuwien/common/entity/Part.java
@@ -8,11 +8,14 @@ public class Part implements Serializable {
     private UUID partId;
     private UUID supplierId;
     private PartType partType;
+    private CaseType caseType;
+    private Color color;
 
     public Part(UUID supplierId, PartType partType) {
         this.partId = UUID.randomUUID();
         this.supplierId = supplierId;
         this.partType = partType;
+        this.color = Color.GRAY;
     }
 
     public UUID getPartId() {
@@ -27,12 +30,30 @@ public class Part implements Serializable {
         return partType;
     }
 
+    public CaseType getCaseType() {
+        return caseType;
+    }
+
+    public void setCaseType(CaseType caseType) {
+        this.caseType = caseType;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
     @Override
     public String toString() {
         return "Part{" +
                 "partId=" + partId +
                 ", supplierId=" + supplierId +
                 ", partType=" + partType +
+                ", caseType=" + caseType +
+                ", color=" + color +
                 '}';
     }
 }
