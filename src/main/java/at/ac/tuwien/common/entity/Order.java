221348f504abcb9d766edf2030ec0186a666ee7f
dinRB <kapreboys6@gmail.com>
2016-01-02 13:53:25 +0100

    initial Order class

---
+++ src/main/java/at/ac/tuwien/common/entity/Order.java
@@ -0,0 +1,53 @@
+package at.ac.tuwien.common.entity;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+/**
+ * Created by Arber on 02.01.2016.
+ */
+public class Order implements Serializable {
+
+    private UUID orderId;
+    private int orderSize;
+    private CaseType caseType;
+    private Color droneColor;
+
+    private Order(Integer orderSize, CaseType caseType, Color droneColor){
+        this.orderId = UUID.randomUUID();
+        this.orderSize = orderSize;
+        this.caseType = caseType;
+        this.droneColor = droneColor;
+    }
+
+    public int getOrderSize() {
+        return orderSize;
+    }
+
+    public void setOrderSize(int orderSize) {
+        this.orderSize = orderSize;
+    }
+
+    public CaseType getCaseType() {
+        return caseType;
+    }
+
+    public void setCaseType(CaseType caseType) {
+        this.caseType = caseType;
+    }
+
+    public Color getDroneColor() {
+        return droneColor;
+    }
+
+    public void setDroneColor(Color droneColor) {
+        this.droneColor = droneColor;
+    }
+
+    @Override
+    public String toString() {
+        return "Order [orderId=" + orderId.toString() + ", orderSize=" + orderSize
+                + ", caseType=" + caseType + ", droneColor=" + droneColor + "]";
+    }
+
+}
