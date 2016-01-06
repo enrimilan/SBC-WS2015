fb059acf5fdf853e3ac5fd93d621f50a2b988417
dinRB <kapreboys6@gmail.com>
2016-01-06 02:57:58 +0100

    adjusted + enhanced bat-scripts for calibrator-robot

--- src/main/java/at/ac/tuwien/bin/rmi/calibrator.bat
+++ src/main/java/at/ac/tuwien/bin/rmi/calibrator.bat
@@ -1,3 +1,6 @@
 @ECHO OFF
-mvn -f ..\..\..\..\..\..\..\..\pom.xml exec:java -Pcalibrator -Dexec.args="rmi"
+ECHO Usage: SERVER_HOST SERVER_PORT %NL%
+set /P host=SERVER_HOST:
+set /P port=SERVER_PORT:
+mvn -f ..\..\..\..\..\..\..\..\pom.xml exec:java -Pcalibrator -Dexec.args="rmi %host% %port%"
 PAUSE
