82a9cc31b47e0fc59bbfa36816e73edb301e1eea
dinRB <kapreboys6@gmail.com>
2016-01-06 02:40:35 +0100

    adjusted + enhanced bat-scripts for supplier-robot

--- src/main/java/at/ac/tuwien/bin/xvsm/supplier.bat
+++ src/main/java/at/ac/tuwien/bin/xvsm/supplier.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
-ECHO.
-set /P usageArg= Usage: PART_TYPE AMOUNT
-ECHO.
-mvn -f ..\..\..\..\..\..\..\..\pom.xml exec:java -Psupplier -Dexec.args="xvsm %usageArg%"
+ECHO Usage: PART_TYPE AMOUNT SERVER_HOST:SERVER_PORT... %NL%
+set /P partType=PART_TYPE:
+set /P amount=AMOUNT:
+set /P serverHost=SERVER_HOST:SERVER_PORT... :
+mvn -f ..\..\..\..\..\..\..\..\pom.xml exec:java -Psupplier -Dexec.args="%partType% %amount% xvsm %serverHost%"
 PAUSE
