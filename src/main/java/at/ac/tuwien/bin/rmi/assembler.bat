0ceb2ea85e02f4791d080ec199eee6c6a7d9ee58
dinRB <kapreboys6@gmail.com>
2016-01-06 02:54:48 +0100

    adjusted + enhanced bat-scripts for assembler-robot

--- src/main/java/at/ac/tuwien/bin/rmi/assembler.bat
+++ src/main/java/at/ac/tuwien/bin/rmi/assembler.bat
@@ -1,3 +1,6 @@
 @ECHO OFF
-mvn -f ..\..\..\..\..\..\..\..\pom.xml exec:java -Passembler -Dexec.args="rmi"
+ECHO Usage: SERVER_HOST SERVER_PORT %NL%
+set /P host=SERVER_HOST:
+set /P port=SERVER_PORT:
+mvn -f ..\..\..\..\..\..\..\..\pom.xml exec:java -Passembler -Dexec.args="rmi %host% %port%"
 PAUSE
--- src/main/java/at/ac/tuwien/bin/xvsm/assembler.bat
+++ src/main/java/at/ac/tuwien/bin/xvsm/assembler.bat
@@ -1,3 +1,6 @@
 @ECHO OFF
-mvn -f ..\..\..\..\..\..\..\..\pom.xml exec:java -Passembler -Dexec.args="xvsm"
+ECHO Usage: SERVER_HOST SERVER_PORT %NL%
+set /P host=SERVER_HOST:
+set /P port=SERVER_PORT:
+mvn -f ..\..\..\..\..\..\..\..\pom.xml exec:java -Passembler -Dexec.args="xvsm %host% %port%"
 PAUSE
